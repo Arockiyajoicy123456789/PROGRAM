9c4c6c0b068bd2c147cf5a94ed15c2282c1c1e2c
Arockiyajoicy123456789 <31499233+Arockiyajoicy123456789@users.noreply.github.com>
2018-01-21 06:21:51 +0000

    Create 45.c

---
+++ 45.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int number,count=0;
+printf("Enter the number:");
+scanf("%d",&number);
+while(number!=0)
+{
+number=number/10;
+count++;
+printf("The count of digit is:%d",count);
+}
+}
