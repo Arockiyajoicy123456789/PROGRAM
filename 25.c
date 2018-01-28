8eb000ab80971e390f47b7d1e1d61379244b3216
Arockiyajoicy123456789 <31499233+Arockiyajoicy123456789@users.noreply.github.com>
2018-01-28 16:41:51 +0530

    Create 25.c

---
+++ 25.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+int i,j,temp,n,a[10];
+printf("Enter the number:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j]i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+for(j=0;j<n;j++)
+{
+printf("%d",a[j]);
+}
+printf("\nTo find the median:");
+n=(n-1)/2;
+printf("%d",a[n]);
+}
