75043232880cef40f5092da26e7618925f963cb3
Arockiyajoicy123456789 <31499233+Arockiyajoicy123456789@users.noreply.github.com>
2018-02-02 16:18:23 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[10]="btabb";
+    int i;
+    for(i=0;a[i]!=0;i++)
+    {
+    if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u')
+    
+     printf("YES");
+    
+    
+    
+    
+}
+}
+
