8ee7be76897016a4ec0f0f0f70a95e5754d490d9
Arockiyajoicy123456789 <31499233+Arockiyajoicy123456789@users.noreply.github.com>
2018-02-05 16:17:23 +0530

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("Enter the number:");
+scanf("%d",&n);
+if((n&(n-1))==0)
+{
+  printf("%d is a power of 2",n);
+}
+else
+{
+  printf("%d is not a power of 2:",n);
+}
+}
