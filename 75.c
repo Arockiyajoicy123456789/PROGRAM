e95faa3478a786c4aa10e52221206d6720ad9dac
Arockiyajoicy123456789 <31499233+Arockiyajoicy123456789@users.noreply.github.com>
2018-02-19 16:05:57 +0530

    Create 75.c

---
+++ 75.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str[100];
+scanf("%S",str);
+int len,i;
+len=strlen(str);
+if(len%2!=0)
+{
+  str[(len-1)/2]='*';
+}
+else
+{
+str[len/2]='*';
+str[(len/2)-1]='*';
+}
+printf("%s",str);
+}
