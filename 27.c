109a3757ab2219caa9936c0361dccf154988c178
Arockiyajoicy123456789 <31499233+Arockiyajoicy123456789@users.noreply.github.com>
2018-03-16 16:30:26 +0530

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int i,j;
+    char a[20];
+    printf("\n enter the string:");
+    scanf("%s",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]>='a'&&a[i]<='z')
+        {
+        a[i]=a[i]-32;
+        }
+        else
+        {
+        a[i]=a[i]+32;
+        }
+    }
+    printf("%s",a);
+}
